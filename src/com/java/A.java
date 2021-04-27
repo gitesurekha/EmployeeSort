950d98e2d1c05107565ca0800989a9cbf90555e9
gitesurekha <surekhagite32@gmail.com>
2021-04-27 12:12:33 +0530

    this is changws in A by dev2

--- src/com/java/A.java
+++ src/com/java/A.java
@@ -3,5 +3,8 @@ package com.java;
 public class A {
 public static void main(String[] args) {
 	System.out.println("hi..this is done by sg162023");
+	
+	
+	System.out.println("hi..this is done by sg162023,,,,,,dev 2 ");
 }
 }
