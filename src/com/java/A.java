4f42f7ef2f0a31f7d6c0818dcfe5f28212e7563b
gitesurekha <surekhagite32@gmail.com>
2021-05-01 17:17:57 +0530

    this is commit from eclipse side

--- src/com/java/A.java
+++ src/com/java/A.java
@@ -1,7 +1,34 @@
 package com.java;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class A {
 public static void main(String[] args) {
+	
+	/*
+	String str=" abc d 123490pqr 54897";
+	Pattern pattern = Pattern.compile("\\w+([0-9]+)\\w+([0-9]+)");
+	Matcher matcher = pattern.matcher(str);
+	for(int i = 0 ; i < matcher.groupCount(); i++) {
+	  matcher.find();
+	  System.out.println(matcher.group());
+
+	
+	
+	*/
+	String line = "This order was32354 placed 343434for 43411 QT ! OK?";
+    String regex = "[^\\d]+";
+
+    String[] str = line.split(regex);
+  //  String required = "";
+    for(String st: str){
+        System.out.println(st);
+    }
+	
+	
+	
+    System.out.println("hi..this is done from eclipse side");
 	System.out.println("hi..this is done by sg162023");
 
 	
