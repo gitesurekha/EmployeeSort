7d7dca6650a6d40472ca2127c30e48e1b276e787
gitesurekha <surekhagite32@gmail.com>
2021-05-01 17:21:34 +0530

    commit to newBr

--- src/com/java/A.java
+++ src/com/java/A.java
@@ -26,6 +26,14 @@ public static void main(String[] args) {
         System.out.println(st);
     }
 	
+    System.out.println("hi..this is done by sg162023");
+
+	System.out.println("hi..this is done by git side   ");
+	System.out.println("hi..this is done by  git side new changes... ");
+	
+	System.out.println("hi..this is done by sg162023,,,,,,dev 2 ");
+	
+	System.out.println("hi..this is done by sg162023,  at 28 .4.2021,,,,,dev 2 ");
 	
 	
     
