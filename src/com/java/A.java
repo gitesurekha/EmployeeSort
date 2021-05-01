db71e8318ef74aff3ec7aa92fb483027f40024ed
gitesurekha <81868255+gitesurekha@users.noreply.github.com>
2021-05-01 17:16:48 +0530

    Update A.java

--- src/com/java/A.java
+++ src/com/java/A.java
@@ -4,7 +4,8 @@ public class A {
 public static void main(String[] args) {
 	System.out.println("hi..this is done by sg162023");
 
-	
+	System.out.println("hi..this is done by git side   ");
+	System.out.println("hi..this is done by  git side new changes... ");
 	
 	System.out.println("hi..this is done by sg162023,,,,,,dev 2 ");
 	
